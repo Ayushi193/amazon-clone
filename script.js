9d0cfab3b9dcda446a407db967984dc6aee4e7d7
Ayushi193 <anandayushishree@gmail.com>
2025-07-01 15:08:11 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const buttons = document.querySelectorAll('.product button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Item added to cart!');
+  });
+});
